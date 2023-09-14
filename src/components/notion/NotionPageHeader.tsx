cb9d53d8a92cdfcd73b7fddc4d4fd6a1dd9d5038
김선민 <tjsals116@gmail.com>
2023-09-14 13:56:46 +0900

    update favicon

--- src/components/notion/NotionPageHeader.tsx
+++ src/components/notion/NotionPageHeader.tsx
@@ -1,4 +1,5 @@
 import { name } from '@/lib/config';
+import Link from 'next/link';
 
 export type NotionPageHeaderProps = React.PropsWithChildren<
   React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement>
@@ -9,7 +10,18 @@ const NotionPageHeader = (propsWithChildren: NotionPageHeaderProps) => {
   return (
     <header className="backdrop-blur-md" {...props}>
       <div className="flex items-center flex-row justify-between gap-12 my-0 mx-auto max-w-5xl">
-        <div className="flex items-center flex-row">{name}</div>
+        <div className="flex items-center flex-row">
+          <Link href="/" className="flex items-center">
+            {/* <Image
+              src="./favicon-96x96.png"
+              className="mr-3 h-6 sm:h-9"
+              width={40}
+              height={40}
+              alt={name}
+            /> */}
+            <span className="self-center text-xl font-semibold">{name}</span>
+          </Link>
+        </div>
         <div className="flex items-center flex-row gap-2">
           <div>ICON-1</div>
           <div>ICON-2</div>
