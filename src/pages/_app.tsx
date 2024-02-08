8de243967469b723e68d7e02dc266d68faf6103a
김선민 <tjsals116@gmail.com>
2024-02-08 23:57:40 +0900

    ♻️ spa 빌드 셋팅

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,18 @@
 import '@/styles/globals.css';
+
 import type { AppProps } from 'next/app';
-import Header from '@/components/layouts/Header';
 import Footer from '@/components/layouts/Footer';
+import Header from '@/components/layouts/Header';
 import Main from '@/components/layouts/Main';
 
 export default function _App({ Component, pageProps }: AppProps) {
   return (
     <Main>
-      <Header />
-      <Component {...pageProps} />
-      <Footer />
+      <>
+        <Header />
+        <Component {...pageProps} />
+        <Footer />
+      </>
     </Main>
   );
 }
