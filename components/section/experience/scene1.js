3cca4d4d15bcb4da698788ebe72019009e9e09c7
김선민 <tjsals116@gmail.com>
2024-03-08 23:55:16 +0900

    ✨ linked the project link to the "Notion" page

--- components/section/experience/scene1.js
+++ components/section/experience/scene1.js
@@ -105,6 +105,7 @@ const ProjectList = [
     description:
       'Google API를 이용하여 Google Workspace에 작성한 내용을 조회하여, 사내 공지사항 및 일정을 대시보드의 형태로 제공하는 시스템을 개발하였습니다.',
     tags: ['Desktap'],
+    link: 'https://gitsunmin.notion.site/dca6ba6ff38042ddadad31b67f3bad1c',
   },
   {
     name: '업무 협업 프로그램 개발',
@@ -115,6 +116,7 @@ const ProjectList = [
     description:
       '문서 공동편집기능을 기반으로 채팅, 할일 관리 등 기능의 협업툴을 개발하였습니다.',
     tags: ['Web', 'B2B'],
+    link: 'https://gitsunmin.notion.site/179c4f41db6d4e51b132738a439e5aa6',
   },
   {
     name: '마켓봄 프로 개발',
@@ -125,6 +127,7 @@ const ProjectList = [
     description:
       '마켓봄 프로의 PC, Mobile 웹 그리고 백오피스 웹 서비스를 초기 단계부터 개발 및 운영업무를 담당하였습니다.',
     tags: ['Web', 'Mobile', 'B2B'],
+    link: 'https://gitsunmin.notion.site/9c437384d105421aafb89ef21ea673a6',
   },
   {
     name: '공통 웹뷰 앱 개발',
@@ -135,6 +138,7 @@ const ProjectList = [
     description:
       '마켓보로에서 사용중인 네이티브 하이브리드 앱들을 Flutter로 재개발하여 공통으로 사용할 수 있는 웹뷰 앱을 개발하였습니다.',
     tags: ['App', 'Flutter'],
+    link: 'https://gitsunmin.notion.site/f098322d80274c32b621e78d1a2588ee',
   },
   {
     name: '식봄, Next.js로 전환',
@@ -145,6 +149,7 @@ const ProjectList = [
     description:
       '사업자 대상 식자재 E-Commerce 서비스인 식봄의 로그인/회원가입 페이지를 PHP에서 Next.js로 전환하는 작업을 하였습니다.',
     tags: ['E-Commerce', 'Next.js'],
+    link: 'https://gitsunmin.notion.site/Next-js-72cf7aca6d73402cbdd8b80556887aa5',
   },
 ];
 
@@ -159,6 +164,7 @@ export default class HistoryScene1 extends HTMLElement {
     this.cards$el = this.shadowRoot.querySelector('.cards');
 
     this.cards$el.innerHTML = ProjectList.map((project) => {
+      console.log('project:', project);
       return /* html */ `
         <li>
           <div class="card">
@@ -172,7 +178,7 @@ export default class HistoryScene1 extends HTMLElement {
             </div>
           </div>
           <div class="links">
-            <a href="#" class="more-link">More</a>
+            <a href="${project.link}" class="more-link">More</a>
           </div>
         </li>
       `;
