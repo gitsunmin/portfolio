d8cbcdf010d30bb2d98cbdd92896f6796dd430e2
gitsunmin <tjsals116@gmail.com>
2024-03-22 15:03:32 +0000

    deploy: 4bdb7045ccf3e528d882bbfef80ab28b1e324e57

---
+++ config.flag.js
@@ -0,0 +1,1 @@
+var $=function(){Object.keys(r.sections).forEach((k)=>{if(!r.sections[k])document.querySelector(`#${k}`)?.remove()})},r={sections:{hello:!0,"about-me":!0,experience:!0,skills:!0}};$();
