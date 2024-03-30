4c867e0efe8e7563e8a8bf2485d8d0aa2a8c5d26
김선민 <tjsals116@gmail.com>
2024-03-30 21:28:27 +0900

    💬 Update project descriptions in listWrapper.ts

--- src/components/section/experience/project/listWrapper.ts
+++ src/components/section/experience/project/listWrapper.ts
@@ -39,7 +39,7 @@ const ProjectList = [
       end: '2019.10',
     },
     description:
-      'Google API를 이용하여 Google Workspace에 작성한 내용을 조회하여, 사내 공지사항 및 일정을 대시보드의 형태로 제공하는 시스템을 개발하였습니다.',
+      'Google API를 이용하여 Google Workspace에 작성한 내용을 조회하여, 사내 공지사항 및 일정을 Digital Signage의 형태로 제공하는 시스템을 개발.',
     tags: ['Desktap'],
     link: 'https://gitsunmin.notion.site/dca6ba6ff38042ddadad31b67f3bad1c',
   },
@@ -50,7 +50,7 @@ const ProjectList = [
       end: '2020.10',
     },
     description:
-      '문서 공동편집기능을 기반으로 채팅, 할일 관리 등 기능의 협업툴을 개발하였습니다.',
+      '문서 공동편집기능을 기반으로 채팅, 할일 관리 등 기능의 협업툴을 개발.',
     tags: ['Web'],
     link: 'https://gitsunmin.notion.site/179c4f41db6d4e51b132738a439e5aa6',
   },
@@ -61,7 +61,7 @@ const ProjectList = [
       end: '2023.08',
     },
     description:
-      '마켓봄 프로의 PC, Mobile 웹 그리고 백오피스 웹 서비스를 초기 단계부터 개발 및 운영업무를 담당하였습니다.',
+      '마켓봄 프로의 PC, Mobile 웹 그리고 백오피스 웹 서비스를 초기 단계부터 개발 및 운영업무를 담당.',
     tags: ['Web', 'Mobile', 'B2B'],
     link: 'https://gitsunmin.notion.site/9c437384d105421aafb89ef21ea673a6',
   },
@@ -72,7 +72,7 @@ const ProjectList = [
       end: '2023.12',
     },
     description:
-      '마켓보로에서 사용중인 네이티브 하이브리드 앱들을 Flutter로 재개발하여 공통으로 사용할 수 있는 웹뷰 앱을 개발하였습니다.',
+      '마켓보로에서 사용중인 네이티브 하이브리드 앱들을 Flutter로 재개발하여 공통으로 사용할 수 있는 웹뷰 앱 개발',
     tags: ['App', 'Flutter', 'Web View'],
     link: 'https://gitsunmin.notion.site/f098322d80274c32b621e78d1a2588ee',
   },
@@ -83,7 +83,7 @@ const ProjectList = [
       end: '2024.03',
     },
     description:
-      '사업자 대상 식자재 E-Commerce 서비스인 식봄의 유지보수 및 Next.js로 전환하는 작업을 하였습니다.',
+      '기존 PHP/CodeIgniter 기반의 식봄 웹 서비스를 현대적인 기술 스택인 Next.js와 Kotlin Spring으로 전환하여 서비스 품질 및 유지보수 효율을 개선',
     tags: ['E-Commerce'],
     link: 'https://gitsunmin.notion.site/Next-js-72cf7aca6d73402cbdd8b80556887aa5',
   },
