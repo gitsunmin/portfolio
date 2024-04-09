d156bebdd29c38d4ed551e7914bb1bf7bad93119
김선민 <tjsals116@gmail.com>
2024-04-09 23:59:51 +0900

    wip: change animation library to react-spring

--- src/components/layout/Section.tsx
+++ src/components/layout/Section.tsx
@@ -1,19 +1,33 @@
-import { motion } from 'framer-motion';
+import { useInView, animated } from '@react-spring/web';
 
 type Props = React.HTMLAttributes<HTMLDivElement>;
 
 export default (props: Props) => {
   const { children, ...others } = props;
-  return (
-    <motion.section
-      whileInView={{
+  const [ref, springs] = useInView(
+    () => ({
+      from: {
+        opacity: 0,
+      },
+      to: {
         opacity: 1,
-        transition: { duration: 1.5 },
-      }}
-      initial={{ opacity: 0 }}
+      },
+      config: {
+        duration: 3.0,
+      },
+    }),
+    {
+      rootMargin: '-20% 0%',
+    }
+  );
+
+  return (
+    <animated.section
+      ref={ref}
+      style={springs}
       className={`flex align-center justify-center w-full`}
     >
       <div {...others}>{children}</div>
-    </motion.section>
+    </animated.section>
   );
 };
