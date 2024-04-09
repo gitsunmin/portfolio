06e74b88677b77c6b05b71589302b6277530091f
김선민 <tjsals116@gmail.com>
2024-04-10 05:49:44 +0900

    Revert "wip: change animation library to react-spring"
    This reverts commit d156bebdd29c38d4ed551e7914bb1bf7bad93119.

--- src/components/layout/Section.tsx
+++ src/components/layout/Section.tsx
@@ -1,33 +1,19 @@
-import { useInView, animated } from '@react-spring/web';
+import { motion } from 'framer-motion';
 
 type Props = React.HTMLAttributes<HTMLDivElement>;
 
 export default (props: Props) => {
   const { children, ...others } = props;
-  const [ref, springs] = useInView(
-    () => ({
-      from: {
-        opacity: 0,
-      },
-      to: {
-        opacity: 1,
-      },
-      config: {
-        duration: 3.0,
-      },
-    }),
-    {
-      rootMargin: '-20% 0%',
-    }
-  );
-
   return (
-    <animated.section
-      ref={ref}
-      style={springs}
+    <motion.section
+      whileInView={{
+        opacity: 1,
+        transition: { duration: 1.5 },
+      }}
+      initial={{ opacity: 0 }}
       className={`flex align-center justify-center w-full`}
     >
       <div {...others}>{children}</div>
-    </animated.section>
+    </motion.section>
   );
 };
