81099442254889564d275c07effc6e77903f6dec
김선민 <tjsals116@gmail.com>
2024-04-27 12:40:36 +0900

    ♻️ Remove unused file

--- src/components/ui/ProfileImage.tsx
+++ src/components/ui/ProfileImage.tsx
@@ -1,16 +1,20 @@
+import Image from 'next/image';
+
 type Props = React.HtmlHTMLAttributes<HTMLImageElement> & {
   alt: string;
 };
 
 export const ProfileImage = ({ alt, className }: Props) => {
   return (
-    <img
+    <Image
       style={{
         boxShadow: '0 0 10px 0 white',
         isolation: 'isolate',
       }}
+      width={200}
+      height={200}
       alt={alt}
-      src="./assets/me.jpeg"
+      src={'/assets/me.jpeg'}
       className={`rounded-full ${className}`}
     />
   );
