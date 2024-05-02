5de3a246a4f34ac7d16ee3d4e083401d4238788b
김선민 <tjsals116@gmail.com>
2024-05-02 21:25:46 +0900

    ✨ Add new scene and update default export in page.tsx

--- src/app/projects/(marketboro)/marketbom-pro/page.tsx
+++ src/app/projects/(marketboro)/marketbom-pro/page.tsx
@@ -1,3 +1,16 @@
-export default function Korens1() {
-  return <>ashdoui</>;
+import { Projector, Scene } from '@/components/layout/Projector';
+import { Scene00 } from '@/components/scenes/projects/marketbom-pro/scene00';
+
+const SCENES: Scene[] = [
+  {
+    id: 'scene00',
+    type: 'intro',
+    description: '포트폴리오 가이드',
+    runtime: 1,
+    component: Scene00,
+  },
+];
+
+export default function Home() {
+  return <Projector scenes={SCENES} />;
 }
