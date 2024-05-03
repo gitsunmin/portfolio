33b0515faae9789558a4b9121efb246fa08c7e26
김선민 <tjsals116@gmail.com>
2024-05-04 07:49:53 +0900

    ♻️ refactor Input of TagSearch to using shadcn

--- src/components/scenes/index/scene03/TagSearch.tsx
+++ src/components/scenes/index/scene03/TagSearch.tsx
@@ -1,7 +1,14 @@
 import React, { useState } from 'react';
-import { Combobox } from '@headlessui/react';
-import { CheckIcon } from '@heroicons/react/24/outline';
-import { match } from 'ts-pattern';
+import {
+  Command,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+  CommandList,
+} from '@/components/ui/command';
+import { Cross1Icon } from '@radix-ui/react-icons';
+import { Badge } from '@/components/ui/badge';
 
 type TagSearchProps = {
   availableTags: string[];
@@ -26,50 +33,6 @@ const Tag = (props: { name: string; onClick: (name: string) => void }) => {
   );
 };
 
-const NothingFound = () => (
-  <div className="relative cursor-default select-none py-2 px-4 text-gray-700">
-    Nothing found.
-  </div>
-);
-
-const TagOption = (props: { tag: string }) => {
-  const { tag } = props;
-
-  return (
-    <Combobox.Option
-      key={tag}
-      className={({ active }) =>
-        `relative cursor-default select-none py-2 pl-10 pr-4 ${
-          active ? 'bg-teal-600 text-white' : 'text-gray-900'
-        }`
-      }
-      value={tag}
-    >
-      {({ selected, active }) => (
-        <>
-          <span
-            className={`block truncate ${
-              selected ? 'font-medium' : 'font-normal'
-            }`}
-          >
-            {tag}
-          </span>
-          {selected ? (
-            <span
-              className={`absolute inset-y-0 left-0 flex items-center pl-3 ${
-                active ? 'text-white' : 'text-teal-600'
-              }`}
-            >
-              b
-              <CheckIcon />
-            </span>
-          ) : null}
-        </>
-      )}
-    </Combobox.Option>
-  );
-};
-
 export const TagSearch: React.FC<TagSearchProps> = ({
   availableTags,
   selectedTags,
@@ -91,28 +54,38 @@ export const TagSearch: React.FC<TagSearchProps> = ({
 
   return (
     <div className="w-full max-w-md p-4">
-      <Combobox value={query} onChange={addTag}>
-        <div className="relative mt-1">
-          <div className="relative w-full cursor-default overflow-hidden rounded bg-white text-left shadow-md focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 focus-visible:ring-offset-2 focus-visible:ring-offset-teal-300 sm:text-sm">
-            <Combobox.Input
-              className="w-full border-none py-2 pl-3 pr-10 text-sm leading-5 text-gray-900 focus:ring-0"
-              displayValue={(tag: string) => tag}
-              onChange={(event) => setQuery(event.target.value)}
-              placeholder="기술 스택을 입력해주세요."
-            />
-          </div>
-          <Combobox.Options className="absolute z-10 mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
-            {match(filteredTags)
-              .with([], NothingFound)
-              .otherwise((tags) =>
-                tags.map((tag) => <TagOption key={tag} tag={tag} />)
-              )}
-          </Combobox.Options>
-        </div>
-      </Combobox>
+      <Command className="rounded-lg border shadow-md">
+        <CommandInput
+          placeholder="기술 스택을 입력해주세요."
+          value={query}
+          onValueChange={setQuery}
+        />
+        <CommandList className="h-[100px]">
+          <CommandEmpty>검색된 기술 스택이 없습니다.</CommandEmpty>
+          <CommandGroup heading>
+            {filteredTags.map((tag) => (
+              <CommandItem
+                key={tag}
+                value={tag}
+                onSelect={() => addTag(tag)}
+                hidden={false}
+                disabled={false}
+                aria-disabled={false}
+              >
+                {tag}
+              </CommandItem>
+            ))}
+          </CommandGroup>
+        </CommandList>
+      </Command>
       <div className="flex flex-wrap gap-2 mt-2">
         {selectedTags.map((tag) => (
-          <Tag key={tag} name={tag} onClick={onRemove} />
+          <Badge key={tag} variant="outline" className="felx gap-[8px]">
+            {tag}
+            <button className="text-white" onClick={() => onRemove(tag)}>
+              <Cross1Icon />
+            </button>
+          </Badge>
         ))}
       </div>
     </div>
